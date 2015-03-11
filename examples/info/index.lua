636a9b382af2dca80a92fb79cdd374d2d3ff4d4a
Alexander Rødseth <rodseth@gmail.com>
2015-03-11 22:46:57 +0100

    Shorter way to decorate the header

--- examples/info/index.lua
+++ examples/info/index.lua
@@ -1,6 +1,6 @@
 content("text/html")
 print([[<!doctype html><html><body style="background: #202020; color: white; font-family: sans-serif; margin: 4em;">]])
-mprint(version().."\n====")
+mprint("# "..version())
 mprint("* HTTP method: "..method())
 mprint("* URL path: "..urlpath())
 --- The HTTP body will only be read once, since it's streamed
