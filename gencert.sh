2f7a6c136eef85b38402ed80968b698524352791
Alexander Rødseth <rodseth@gmail.com>
2015-03-13 22:24:43 +0100

    Fixed a typo

--- gencert.sh
+++ gencert.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # For generating SSL certs, for testing.
-# Just press return at all the prompt, but enter "localhost" at Common Name.
+# Just press return at all the prompts, but enter "localhost" at Common Name.
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 9999 -nodes
