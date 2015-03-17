8fea567e2799f08e30387087388f0f7582ab7c37
Alexander F Rødseth <rodseth@gmail.com>
2015-03-17 15:34:13 +0100

    Initial support for simpleredis

---
+++ index.lua
@@ -0,0 +1,2 @@
+print("hi")
+print(newset())
--- redis.go
+++ redis.go
@@ -8,7 +8,8 @@ import (
 	"github.com/yuin/gopher-lua"
 )
 
-// TODO: Use LUserData for storing the simpleredis types in Lua
+// Look at fileMethods in gopher-lua's iolib.go for a way to
+// expose datastructures to lua.
 
 /*
 
@@ -26,18 +27,53 @@ function Set:add (name)
 end
 */
 
+func setToString(L *lua.LState) int {
+	//set := checkSet(L)
+	//if set.Type() == lSet {
+	//	L.Push(LString(set.name))
+	//} else {
+	//	L.Push(LString(""))
+	//}
+	L.Push(lua.LString("untitled"))
+	return 1
+}
+
+func setSet(L *lua.LState) int {
+	return 0
+}
+
+func setGet(L *lua.LState) int {
+	return 0
+}
+
+var setMethods = map[string]lua.LGFunction{
+	"__tostring": setToString,
+	"set": setSet,
+	"get": setGet,
+}
+
+func setNew(L *lua.LState) {
+	mt := L.NewTypeMetatable(lSetClass)
+	mt.RawSetH(LString("__index:"), mt)
+	L.SetFuncs(mt, setMethods)
+	//mt.RawSetH(LString("lines"), L.NewClosure(fileLines, L.NewFunction(fileLinesIter)))
+}
+
 // Make functions related to HTTP requests and responses available to Lua scripts
 func exportRedisFunctions(w http.ResponseWriter, req *http.Request, L *lua.LState, userstate *permissions.UserState) {
 	pool := userstate.Pool()
 	dbindex := userstate.DatabaseIndex()
 
 	// Return a Set in Lua
-	L.SetGlobal("NewSet", L.NewFunction(func(L *lua.LState) int {
+	L.SetGlobal("newset", L.NewFunction(func(L *lua.LState) int {
 		name := L.ToString(1)
 		set := simpleredis.NewSet(pool, name)
 		set.SelectDatabase(dbindex)
 
-		return 0 // number of results
+		//
+		ud := lua.LUserData(&set)
+		L.Push(ud)
+		return 1 // number of results
 	}))
 
 	// TODO: Find a way to store the Go simpleredis Set in lua. Perhaps as a pointer.
