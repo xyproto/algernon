ee1a959343b07396b99ee1657a4e12dd59376a04
Alexander Rødseth <rodseth@gmail.com>
2015-03-24 16:07:08 +0100

    Minor change

--- banner.go
+++ banner.go
@@ -4,5 +4,5 @@ package main
 
 // Return the server name, version number and description
 func banner() string {
-	return "\n" + version_string + "\nHTTP/2 web server\n\n"
+	return "\n" + version_string + "\nHTTP/2 web server\n"
 }
