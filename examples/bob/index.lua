44641ee84fa9d8a6789a9949917eefb1668debe0
Alexander Rødseth <rodseth@gmail.com>
2015-03-25 20:03:07 +0100

    Fixed a typo

--- examples/bob/index.lua
+++ examples/bob/index.lua
@@ -2,7 +2,7 @@
 print("Has user bob: " .. tostring(HasUser("bob")))
 print("Logged in on server: " .. tostring(IsLoggedIn("bob")))
 print("Is confirmed: " .. tostring(IsConfirmed("bob")))
-print("Username stored in cookies (or blank): " .. Username())
+print("Username stored in cookie (or blank): " .. Username())
 print("Current user is logged in, has a valid cookie and *user rights*: " .. tostring(UserRights()))
 print("Current user is logged in, has a valid cookie and *admin rights*: " .. tostring(AdminRights()))
 print("Try: /register, /confirm, /remove, /login, /logout, /data, /makeadmin and /admin")
