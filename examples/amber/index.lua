241c55f1b387c649c6d9a3c2c07581f1d29667b6
Alexander Rødseth <rodseth@gmail.com>
2015-03-25 22:29:14 +0100

    Added an Amber example

---
+++ examples/amber/index.lua
@@ -0,0 +1,19 @@
+aprint([[
+
+$css = "]] .. "/" .. scriptdir("css/style.css") .. [["
+$user = "]] .. Username() .. [["
+
+doctype 5
+html
+    head
+        title Amber
+        link[href=$css][rel="stylesheet"][type="text/css"]
+    body
+        h1 Hi, #{$user}
+        div#content
+            p
+                | This text goes into the first paragraph.
+                |  As well as this text. And this. Yarr.
+
+            a[href="/"] Back to main
+]])
