05f30800ad84acebaa49e72e5c6c908a8c2eca86
Alexander F Rødseth <rodseth@gmail.com>
2015-04-16 14:46:50 +0200

    Reorganized the examples

--- examples/counter/data.lua
+++ examples/counter/data.lua
@@ -6,10 +6,11 @@
 -- may return one value that will be converted to string.
 --
 
+title = "Counter Example"
+
 -- Count from 0 and up. Return the current value.
 function counter()
-  -- Use the "pageview" KeyValue and increase the "counter" key with 1
+  -- Use the "pageview" KeyValue and increase the "counter" value with 1
   return KeyValue("pageview"):inc("counter")
 end
-
 
