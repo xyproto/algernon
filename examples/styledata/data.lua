3f2e9731c5d293d107c137f70fd566a53fd281d3
Alexander F Rødseth <rodseth@gmail.com>
2015-04-16 11:50:38 +0200

    New example

---
+++ examples/styledata/data.lua
@@ -0,0 +1,18 @@
+--
+-- Functions in this file are available to call from 
+-- the amber.index file in the same directory.
+--
+-- The functions may take any number of strings and
+-- may return one value that will be converted to string.
+--
+
+title = "Counter Example"
+font = "Lato"
+
+-- Count from 0 and up. Return the current value.
+function counter()
+  -- Use the "counter" KeyValue and increase the "number" key with 1
+  return KeyValue("counter"):inc("number")
+end
+
+
