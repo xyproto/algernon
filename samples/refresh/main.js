abad425de116d356bfc29c20cba9c04011fb12ab
Alexander F Rødseth <rodseth@gmail.com>
2015-05-12 15:51:26 +0200

    Removed samples that were only used for testing

--- samples/refresh/main.js
+++
@@ -1,9 +0,0 @@
-if (!!window.EventSource) {
-  var host = window.location.host;
-  if (host.indexOf(':') >= 0) { host = host.split(':')[0] }
-  var source = new EventSource(window.location.protocol + '//' + host + ':5553/fs');
-  source.addEventListener('message', function(e) {
-    const path = '/' + e.data
-    if (path.indexOf(window.location.pathname) >= 0) { location.reload() }
-  }, false);
-}
