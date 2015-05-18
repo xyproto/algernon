de455e666dbe2acd415a5e2bd7b631edeed7402f
Alexander F Rødseth <rodseth@gmail.com>
2015-05-18 13:05:26 +0200

    Fixed a typo. Better comments.

--- samples/events/index.lua
+++ samples/events/index.lua
@@ -8,34 +8,34 @@ setheader("Connection", "keep-alive")
 setheader("Access-Control-Allow-Origin", "*")
 
 function event(message)
-  log("EVENT: " .. message)
+  log("streaming event: " .. message)
   print("data: " .. message .. "\n")
   flush()
 end
 
 function finish()
-  log("Done streaming events")
   print("\n")
   flush()
 end
 
 function main()
-  log("Steaming events")
+  log("Streaming events")
   log("Turn off buffering for this to work properly (turn off debug mode)")
 
   -- The following must take < 10 seconds, before the request times out
   event("one event")
-  sleep(2)
+  sleep(0.2)
   event("a second event")
   sleep(1)
   event("a third event")
-  sleep(0.2)
+  sleep(2)
   event("yet another event")
   sleep(1)
   event("eventorama!")
   sleep(3)
   event("done")
   finish()
+  log("Done streaming")
 end
 
 main()
