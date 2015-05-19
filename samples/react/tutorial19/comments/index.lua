2ea3159486b7e69b4ac9e35f317198411711f723
Alexander F Rødseth <rodseth@gmail.com>
2015-05-19 11:55:23 +0200

    Minor addition

--- samples/react/tutorial19/comments/index.lua
+++ samples/react/tutorial19/comments/index.lua
@@ -1,6 +1,9 @@
 content("application/javascript")
+setheader("Cache-Control", "no-cache")
+
 if method() == "POST" then
   local data = formdata()
+
   -- To be implemented
   -- See https://github.com/reactjs/react-tutorial/blob/master/server.go
   log("author: "..data["author"])
