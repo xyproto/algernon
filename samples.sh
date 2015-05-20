6635c39449653513f5d7b49ad91988f0687181f6
Alexander F Rødseth <rodseth@gmail.com>
2015-05-20 15:27:26 +0200

    Remove old sample script

--- samples.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo 'Please visit http://localhost:3000/'
-./algernon -conf server.lua -dir samples "$@" -httponly -autorefresh
--- servebob.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo 'Please visit https://localhost:3000/'
-./algernon -conf server.lua -dir samples/bob "$@"
