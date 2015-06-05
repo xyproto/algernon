66ae82a9a3402b1d6688ee6f3ab3ce6a9ec40af6
Alexander F Rødseth <rodseth@gmail.com>
2015-06-05 11:43:10 +0200

    Created working Homebrew package

--- system/homebrew/algernon.rb
+++ system/homebrew/algernon.rb
@@ -1,17 +1,13 @@
 require "formula"
 
 class Algernon < Formula
+  desc "HTTP/2 web server with Lua support"
   homepage "http://algernon.roboticoverlords.org/"
   url "https://github.com/xyproto/algernon/archive/0.71.tar.gz"
-  sha1 "8da6f60cbcab9eee68fe9483f23338a0ca1ec196"
+  version "0.73"
+  sha256 "1d52a4073c32db9656284b0cfbe7aba72233d33c9317803bf322d1bf3b252382"
 
   head "https://github.com/xyproto/algernon.git"
-
-  bottle do
-    sha1 "1189a998e33951ea6c5a4577b92d5b9090c1d2fe" => :mavericks
-    sha1 "765981eef07f07a947cb31a4eaa17962ae945b21" => :mountain_lion
-    sha1 "a7f5387d0e3595912dc45bd6f74811b2c4431d45" => :lion
-  end
 
   depends_on "go" => :build
   depends_on :hg => :build
