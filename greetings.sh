12e2e94c1cc32c912505c5702b34b291f621293e
Alexander F Rødseth <rodseth@gmail.com>
2015-06-05 13:33:00 +0200

    Minor change to one of the sample scripts

--- greetings.sh
+++ greetings.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo 'Please visit http://localhost:3000/'
-./algernon --conf serverconf.lua --dir samples/greetings --httponly --debug --autorefresh --bolt --server "$@"
+./algernon --dev --conf serverconf.lua --dir samples/greetings --httponly --debug --autorefresh --bolt --server "$@"
