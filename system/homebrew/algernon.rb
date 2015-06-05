b8bf7389fa2026cae6db5a0697d4ee4cf4413b42
Alexander F Rødseth <rodseth@gmail.com>
2015-06-05 16:08:25 +0200

    Updated the sha256

--- system/homebrew/algernon.rb
+++ system/homebrew/algernon.rb
@@ -3,7 +3,7 @@ class Algernon < Formula
   homepage "http://algernon.roboticoverlords.org/"
   url "https://github.com/xyproto/algernon/archive/0.74.tar.gz"
   version "0.74"
-  sha256 "eb6cbdad6c497f06732badaf837a01dc13272dd31420c688a0f297136553db5e"
+  sha256 "1341af6864643a968d85bfa63ca231604b6d1123919c6826ae179908c6c4a176"
 
   head "https://github.com/xyproto/algernon.git"
 
