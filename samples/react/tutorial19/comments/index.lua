c14d5b44ebc68129204288c421679cafae89cac0
Alexander F Rødseth <rodseth@gmail.com>
2015-06-17 18:37:48 +0200

    Reverting

---
+++ samples/react/tutorial19/comments/index.lua
@@ -0,0 +1,13 @@
+content("application/javascript")
+setheader("Cache-Control", "no-cache")
+
+-- Use a Redis list for the comments
+comments = List("comments")
+
+if method() == "POST" then
+  -- Add the form data to the comment list, as JSON
+  comments:add(ToJSON(formdata()))
+else
+  -- Combine all the JSON comments to a JSON document
+  print("["..table.concat(comments:getall(), ",").."]")
+end
