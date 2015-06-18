78e5c318e2b2bc07fd15bc103f7b8e86f615cd9e
Alexander F Rødseth <rodseth@gmail.com>
2015-06-18 23:37:16 +0200

    Better logging for the `serve` function

--- servelua.go
+++ servelua.go
@@ -1,23 +1,30 @@
 package main
 
 import (
+	log "github.com/sirupsen/logrus"
 	"github.com/xyproto/pinterface"
 	"github.com/yuin/gopher-lua"
 	"net/http"
 	"path/filepath"
 )
 
-// Expose functions for serving other files to Lua (and caching)
+// Expose functions for serving other files to Lua
 func exportServeFile(w http.ResponseWriter, req *http.Request, L *lua.LState, filename string, perm pinterface.IPermissions, luapool *lStatePool, cache *FileCache) {
 
 	// Serve a file in the scriptdir
 	L.SetGlobal("serve", L.NewFunction(func(L *lua.LState) int {
 		scriptdir := filepath.Dir(filename)
 		serveFilename := filepath.Join(scriptdir, L.ToString(1))
-		if exists(serveFilename) {
-			filePage(w, req, serveFilename, perm, luapool, cache)
+		if !exists(serveFilename) {
+			log.Error("Could not serve " + serveFilename + ". File not found.")
+			return 0 // Number of results
 		}
-		return 0
+		if isDir(serveFilename) {
+			log.Error("Could not serve " + serveFilename + ". Not a file.")
+			return 0 // Number of results
+		}
+		filePage(w, req, serveFilename, perm, luapool, cache)
+		return 0 // Number of results
 	}))
 
 }
