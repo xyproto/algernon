d37fa8c03d5dbaca0bfcd00f32f1169a8fad03e2
Alexander F Rødseth <rodseth@gmail.com>
2015-09-14 18:06:15 +0200

    Script for the `welcome` example

---
+++ welcome.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo 'Try editing the markdown file in samples/welcome and see the'
+echo 'results instantly in the browser at http://localhost:3000/'
+./algernon --dev --conf serverconf.lua --dir samples/welcome --httponly --debug --autorefresh --bolt --server "$@"
