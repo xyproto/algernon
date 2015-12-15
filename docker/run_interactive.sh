a47e0a768e3665b9098e676d50743ac1b9eb6e32
Alexander F Rødseth <rodseth@gmail.com>
2015-12-15 10:45:48 +0100

    Add Dockerfile for using Algernon interactively

---
+++ docker/run_interactive.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# The --publish argument first takes the local port and then the container port
+# The -v argument first takes the local directory and then the container directory name
+docker run -v `pwd`/serve:/srv/algernon -v `pwd`/config:/etc/algernon -i --rm --publish 4000:4000 algernon_interactive
