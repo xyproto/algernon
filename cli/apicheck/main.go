7eb9187db857d3e7e35721c58d659c7fa8cc63bf
Alexander F Rødseth <rodseth@gmail.com>
2016-01-25 23:03:59 +0100

    Make golint happy

--- cli/apicheck/main.go
+++ cli/apicheck/main.go
@@ -1,7 +1,7 @@
+// API version number check
 package main
 
 import (
-	"errors"
 	"fmt"
 	"github.com/xyproto/permissionbolt"
 	"github.com/xyproto/permissions2"
@@ -12,26 +12,27 @@ import (
 	"github.com/xyproto/simpleredis"
 )
 
-// API version number check
-
+// VersionInfo helps to keep track of package names and versions
 type VersionInfo struct {
 	name    string
 	current float64
 	target  float64
 }
 
+// New takes the name of the go package, the current and the desired version
 func New(name string, current, target float64) *VersionInfo {
 	return &VersionInfo{name, current, target}
 }
 
+// Check compares the current and target version
 func (v *VersionInfo) Check() error {
 	if v.current != v.target {
-		return errors.New(fmt.Sprintf("is %.1f, needs version %.1f",
-			v.current, v.target))
+		return fmt.Errorf("is %.1f, needs version %.1f", v.current, v.target)
 	}
 	return nil
 }
 
+// Status reports if the current version is satisfactory
 func (v *VersionInfo) Status() {
 	fmt.Print("\t" + v.name + "...")
 	if err := v.Check(); err != nil {
