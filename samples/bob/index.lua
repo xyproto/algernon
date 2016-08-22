edc94c70e7ac3f8b40d97b92ac7ebd26c3722f16
Alexander F Rødseth <rodseth@gmail.com>
2016-08-22 16:01:03 +0200

    Add notification when running the permissions sample from an unsupported directory

--- samples/bob/index.lua
+++ samples/bob/index.lua
@@ -6,3 +6,7 @@ print("Username stored in cookie (or blank): " .. Username())
 print("Current user is logged in, has a valid cookie and *user rights*: " .. tostring(UserRights()))
 print("Current user is logged in, has a valid cookie and *admin rights*: " .. tostring(AdminRights()))
 print("Try: /register, /confirm, /remove, /login, /logout, /clear, /data, /makeadmin and /admin")
+
+if urlpath() ~= "/" then
+  print[[NOTE: The current URL path is not "/". For the default URL permissions to work, Algernon must either be run from this direcotry, or the URL prefixes must be configured correctly]]
+end
