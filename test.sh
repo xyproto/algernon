6cb1564933c6b5328de7e3347b3537baf4184b8e
Alexander F Rødseth <rodseth@gmail.com>
2017-04-26 14:12:39 +0200

    Add test of functionality

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo -ne 'Testing HTTP server...\t'
+./algernon --quiet --httponly --server --boltdb /tmp/_bolt_test.db --addr :45678 &
+sleep 1
+output=$(curl -sIm3 -o- http://localhost:45678)
+if [[ $output == *"Server: Algernon"* ]]; then
+  echo ok
+else
+  echo FAIL
+  exit 1
+fi
