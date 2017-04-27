0af9e5d836ba6b06cdc10121419f065839364c4a
Alexander F Rødseth <rodseth@gmail.com>
2017-04-27 21:03:35 +0200

    Update comments

--- check.sh
+++ check.sh
@@ -12,12 +12,14 @@ go vet && echo ok || echo fail
 echo -n golint...
 golint && echo ok || echo fail
 
-# tools from https://github.com/dominikh/go-tools
+# go get honnef.co/go/tools/cmd/gosimple
 echo -n gosimple...
 gosimple && echo ok || echo fail
 
+# go get honnef.co/go/tools/cmd/unused
 echo -n unused...
 unused && echo ok || echo fail
 
+# go get honnef.co/go/tools/cmd/staticcheck
 echo -n staticcheck...
 staticcheck && echo ok || echo fail
