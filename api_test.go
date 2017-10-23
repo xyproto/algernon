0c2ca8cd2233977816e29567a2c463121adc0d2a
Alexander F Rødseth <rodseth@gmail.com>
2017-10-23 21:02:59 +0200

    Update API test and vendored dependencies

--- api_test.go
+++ api_test.go
@@ -42,8 +42,8 @@ func (v *VersionInfo) Check() error {
 func TestAPI(t *testing.T) {
 	assert.Equal(t, New("simplebolt", simplebolt.Version, 3.0).Check(), nil)
 	assert.Equal(t, New("permissionbolt", permissionbolt.Version, 2.1).Check(), nil)
-	assert.Equal(t, New("simpleredis", simpleredis.Version, 2.1).Check(), nil)
-	assert.Equal(t, New("permissions", permissions.Version, 2.3).Check(), nil)
+	assert.Equal(t, New("simpleredis", simpleredis.Version, 2.3).Check(), nil)
+	assert.Equal(t, New("permissions", permissions.Version, 2.5).Check(), nil)
 	//assert.Equal(t, New("simplemaria", simplemaria.Version, 3.0).Check(), nil)
 	//assert.Equal(t, New("permissionsql", permissionsql.Version, 2.0).Check(), nil)
 	//assert.Equal(t, New("simplehstore", simplehstore.Version, 2.3).Check(), nil)
