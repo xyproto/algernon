705b044e9fcd642c45d0f4e49cf039d1d800f9e0
Alexander F Rødseth <rodseth@gmail.com>
2017-10-26 14:06:02 +0200

    Reorder index files

--- engine/dirhandler.go
+++ engine/dirhandler.go
@@ -16,7 +16,7 @@ import (
 
 var (
 	// List of filenames that should be displayed instead of a directory listing
-	indexFilenames = []string{"index.lua", "index.html", "index.md", "index.txt", "index.pongo2", "index.amber", "index.tmpl", "index.po2", "index.happ", "index.hyper", "index.hyper.js", "index.hyper.jsx"}
+	indexFilenames = []string{"index.lua", "index.html", "index.md", "index.txt", "index.pongo2", "index.tmpl", "index.po2", "index.amber", "index.happ", "index.hyper", "index.hyper.js", "index.hyper.jsx"}
 
 	doubleP  = utils.Pathsep + utils.Pathsep /* // */
 	dotSlash = "." + utils.Pathsep           /* ./ */
