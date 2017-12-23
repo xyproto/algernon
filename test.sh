b9d182a0bba711212c9567171d14cc0818b4ee15
Alexander F Rødseth <rodseth@gmail.com>
2017-12-23 15:30:25 +0100

    Improve robustness of test script

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -ne 'Testing HTTP server...\t'
 ./algernon --quiet --httponly --server --boltdb /tmp/_bolt_test.db --addr :45678 &
-sleep 2
+sleep 10
 output=$(curl -sIm3 -o- http://localhost:45678)
 if [[ $output == *"Server: Algernon"* ]]; then
   echo ok
@@ -9,3 +9,4 @@ else
   echo FAIL
   exit 1
 fi
+kill $(ps -ef | grep algernon | grep :45678 | cut -d" " -f4)
