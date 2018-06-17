ef241fd1a41c400d6574fbc3338969284fdac095
Alexander F Rødseth <rodseth@gmail.com>
2018-06-17 23:55:15 +0200

    Add welcome.bat, for Windows developers

---
+++ welcome.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Try editing the markdown file in samples/welcome and see the
+echo results instantly in the browser at http://localhost:3000/
+.\algernon.exe --dev --conf serverconf.lua --dir samples\welcome --httponly --debug --autorefresh --bolt --server
