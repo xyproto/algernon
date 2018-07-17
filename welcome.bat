6a6592e9a1137366da42ca36cba048a8b398aba8
Alexander F Rødseth <rodseth@gmail.com>
2018-07-17 04:25:47 -0700

    Update bat file

--- welcome.bat
+++ welcome.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Try editing the markdown file in samples/welcome and see the
 echo results instantly in the browser at http://localhost:3000/
-.\algernon.exe --dev --conf serverconf.lua --dir algernon-1.10\samples --httponly --debug --autorefresh --bolt --server
+.\algernon.exe --dev --conf serverconf.lua --dir samples/welcome --httponly --debug --autorefresh --bolt --server
