84f60422987288a5a4a442828432cbe7309ba819
Alexander F. Rødseth <rodseth@gmail.com>
2018-10-05 00:27:47 +0200

    Better test script

--- test.sh
+++ test.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-echo -ne 'Testing HTTP server...\t'
-./algernon --quiet --httponly --server --boltdb /tmp/_bolt_test.db --addr :45678 &
-sleep 10
+echo -ne 'Starting Algernon...\t'
+./algernon --quiet --httponly --server --nodb --addr :45678 &
+PID=$!
+function finish {
+  echo -ne "Stopping PID $PID...\t"
+  kill $PID 2>/dev/null && echo ok || echo fail
+}
+trap finish EXIT
+echo ok
+echo -ne 'Checking response...\t'
+for i in 1 2 3 4 5; do curl -sIm3 -o/dev/null http://localhost:45678 && break || sleep 1; done
 output=$(curl -sIm3 -o- http://localhost:45678)
 if [[ $output == *"Server: Algernon"* ]]; then
   echo ok
 else
-  echo FAIL
+  echo fail
   exit 1
 fi
-kill $(ps -ef | grep algernon | grep :45678 | cut -d" " -f4) 2>/dev/null
-exit 0
