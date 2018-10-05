a4f2491ef09551290461cac8a361680a18db30f7
Alexander F. Rødseth <rodseth@gmail.com>
2018-10-05 11:37:22 +0200

    Better test script

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -ne 'Starting Algernon...\t'
+echo -ne 'Launching Algernon...\t'
 ./algernon --quiet --httponly --server --nodb --addr :45678 &
 PID=$!
 function finish {
@@ -8,8 +8,8 @@ function finish {
 }
 trap finish EXIT
 echo ok
-echo -ne 'Checking response...\t'
-for i in 1 2 3 4 5; do curl -sIm3 -o/dev/null http://localhost:45678 && break || sleep 1; done
+echo -ne 'Waiting for response...\t'
+for i in $(seq 1 30); do curl -sIm3 -o/dev/null http://localhost:45678 && break || sleep 1; done
 output=$(curl -sIm3 -o- http://localhost:45678)
 if [[ $output == *"Server: Algernon"* ]]; then
   echo ok
