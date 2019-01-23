36423389a156af0a216a85c51b2ba4709f538e4b
Alexander F. Rødseth <rodseth@gmail.com>
2019-01-23 22:18:21 +0100

    Update the regression tests

--- regtest/issue13/serve.sh
+++ regtest/issue13/serve.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-elementary_iso_file=~/download/elementaryos-5.0-stable.20181016.iso
-if [ ! -f "$elementary_iso_file" ]; then
-  echo 'Please modifiy serve.sh so that it serves a directory with a really large file'
-  exit 1
-fi
-mkdir -p /tmp/pub
-ln -sf "$elementary_iso_file" /tmp/pub/elementary.iso
-go build && ./algernon -x  /tmp/pub
+cd "$(dirname $(readlink -f $0))"
+truncate -s 10T large-file
+(cd ../..; go build && ./algernon -x regtest/issue13)
--- regtest/issue7/serve.sh
+++ regtest/issue7/serve.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-go build && ./algernon --server --nolimit --addr=:3001 regtest/issue7/server.lua
+cd "$(dirname $(readlink -f $0))"
+(cd ../..; go build && ./algernon --server --nolimit --addr=:3001 regtest/issue7/server.lua)
