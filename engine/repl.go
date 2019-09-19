d21954ec45c548b26dff6d1b39165187aeb28ef1
Alexander F. Rødseth <rodseth@gmail.com>
2019-09-19 10:58:47 +0200

    Format repl.go with go fmt

--- engine/repl.go
+++ engine/repl.go
@@ -17,9 +17,9 @@ import (
 	"github.com/xyproto/algernon/lua/datastruct"
 	"github.com/xyproto/algernon/lua/jnode"
 	"github.com/xyproto/algernon/lua/pure"
+	"github.com/xyproto/ask"
 	"github.com/xyproto/gopher-lua"
 	"github.com/xyproto/textoutput"
-	"github.com/xyproto/ask"
 )
 
 const (
