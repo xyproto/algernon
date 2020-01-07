ea84333ce16c1b3c81908fd2d48b5562c5bdb1bc
Alexander F. Rødseth <rodseth@gmail.com>
2020-01-07 15:40:35 +0100

    Update release script

--- release.sh
+++ release.sh
@@ -8,14 +8,17 @@ echo 'Compiling...'
 export GOARCH=amd64
 echo '* Linux'
 GOOS=linux go build -o $name.linux
+echo '* Linux static w/ upx'
+CGO_ENABLED=0 GOOS=linux go build -v -trimpath -ldflags "-s" -a -o $name.linux_static
+upx $name.linux_static
+echo '* Plan9'
+GOOS=plan9 go build -o $name.plan9
 echo '* macOS'
 GOOS=darwin go build -o $name.macos
 echo '* FreeBSD'
 GOOS=freebsd go build -o $name.freebsd
 echo '* NetBSD'
 GOOS=netbsd go build -o $name.netbsd
-echo '* Dragonfly'
-GOOS=dragonfly go build -o $name.dragonfly
 echo '* OpenBSD'
 GOOS=openbsd go build -o $name.openbsd
 echo '* Windows'
@@ -24,10 +27,6 @@ echo '* Linux ARM64'
 GOOS=linux GOARCH=arm64 go build -o $name.linux_arm64
 echo '* RPI 2/3'
 GOOS=linux GOARCH=arm GOARM=6 go build -o $name.rpi
-
-# Currently does not build for plan9 because of the fsnotify package
-#echo '* Plan9'
-#GOOS=plan9 go build -o $name.plan9
 
 # Compress the Windows release
 echo "Compressing $name-$version.zip"
@@ -38,7 +37,7 @@ rm -r "$name-$version"
 rm $name.exe
 
 # Compress the Linux releases with xz
-for p in linux linux_arm64 rpi; do
+for p in linux linux_arm64 rpi linux_static; do
   echo "Compressing $name-$version.$p.tar.xz"
   mkdir "$name-$version-$p"
   cp $name.$p LICENSE "$name-$version-$p/"
@@ -48,7 +47,7 @@ for p in linux linux_arm64 rpi; do
 done
 
 # Compress the other tarballs with gz
-for p in macos freebsd netbsd dragonfly openbsd; do
+for p in macos freebsd netbsd openbsd plan9; do
   echo "Compressing $name-$version.$p.tar.gz"
   mkdir "$name-$version-$p"
   #cp $name.1 "$name-$version-$p/"
