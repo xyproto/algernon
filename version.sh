f1c25514e64bafbbd1b6428ee94c5aa2c38d4649
Alexander F. Rødseth <rodseth@gmail.com>
2021-08-23 14:24:39 +0200

    Add a version script

---
+++ version.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+#
+# Self-modifying script that updates the version numbers
+#
+# Requires the `setconf` utility.
+#
+
+# The current version goes here, as the default value
+VERSION=${1:-'1.12.12'}
+
+if [ -z "$1" ]; then
+  echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
+  exit 0
+fi
+
+echo "Setting the version to $VERSION"
+
+# Set the version in various files
+setconf README.md '* Version' $VERSION
+setconf main.go versionString "\"Algernon "$VERSION"\""
+
+# Update the date and version in the man page
+d=$(LC_ALL=C date +'%d %b %Y')
+sed -i "s/\"[0-9]* [A-Z][a-z]* [0-9]*\"/\"$d\"/g" algernon.1
+sed -i "s/[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/$VERSION/g" algernon.1
+
+# Update the version in this script
+sed -i "s/[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/$VERSION/g" "$0"
