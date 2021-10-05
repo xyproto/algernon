9885f7c948fb4b76a7def7ef77083666e8d5cd1a
Alexander F. Rødseth <rodseth@gmail.com>
2021-10-05 12:59:36 +0200

    Use strings.Builder when opening an URL

--- engine/url.go
+++ engine/url.go
@@ -2,20 +2,29 @@ package engine
 
 import (
 	"os/exec"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 )
 
 // OpenURL tries to open an URL with the system browser
-func (ac *Config) OpenURL(host, colonPort string, httpsPrefix bool) {
-	if host == "" {
-		host = "localhost"
-	}
-	protocol := "http://"
+func (ac *Config) OpenURL(host, cPort string, httpsPrefix bool) {
+	// Build the URL
+	var sb strings.Builder
 	if httpsPrefix {
-		protocol = "https://"
+		sb.WriteString("https://")
+	} else {
+		sb.WriteString("http://")
 	}
-	url := protocol + host + colonPort
+	if host == "" {
+		sb.WriteString("localhost")
+	} else {
+		sb.WriteString(host)
+	}
+	sb.WriteString(cPort)
+	url := sb.String()
+
+	// Open the URL
 	log.Info("Running: " + ac.openExecutable + " " + url)
 	cmd := exec.Command(ac.openExecutable, url)
 	cmd.Run()
