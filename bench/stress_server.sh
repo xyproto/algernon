a54d9ef87e1b161fe447ad04edc3b9172bcea625
Alexander F. Rødseth <rodseth@gmail.com>
2022-03-28 14:24:53 +0200

    Modify the benchmark script

--- bench/stress_server.sh
+++ bench/stress_server.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# The "-race" flag gets in the way of the CPU profiling
-go build -mod=vendor && ./algernon -t -c --cachesize=10000000 --nolimit --cpuprofile=algernon.prof . :7531
-#go build -race && ./algernon -t -c --cachesize=10000000 --nolimit . :7531
-echo 'Now run: "go tool pprof algernon algernon.prof" and type "weblist"'
+
+go build -mod=vendor && ./algernon -n -t -c --cachesize=10000000 --nolimit --cpuprofile=algernon.prof . :7531
+
+# The "-race" flag gets in the way of the CPU profiling, unfortunately
+#go build -mod=vendor -race && ./algernon -n -t -c --cachesize=10000000 --nolimit . :7531
+
+echo 'Now run: "go tool pprof algernon algernon.prof" and try "top50"'
