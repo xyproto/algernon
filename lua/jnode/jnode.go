86c3a7c458770c07fc644467959989ccee844384
Alexander F. Rødseth <rodseth@gmail.com>
2022-07-02 16:57:07 +0200

    Check if the Lua table sent to the json function is empty
    Fixes #106

--- lua/jnode/jnode.go
+++ lua/jnode/jnode.go
@@ -9,9 +9,9 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/xyproto/jpath"
 	"github.com/yuin/gluamapper"
 	lua "github.com/yuin/gopher-lua"
-	"github.com/xyproto/jpath"
 )
 
 const (
@@ -389,6 +389,12 @@ func LoadJSONFunctions(L *lua.LState) {
 		table := L.ToTable(1)
 		if table == nil {
 			L.ArgError(1, "Expected a table as the first argument")
+		}
+
+		// Is the given table empty?
+		if table.Len() == 0 {
+			L.Push(lua.LString("{}"))
+			return 1 // number of results
 		}
 
 		// Convert the Lua table to a map that can be used when converting to JSON (map[string]interface{})
