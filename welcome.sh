4fffd85e66b00ef50ce8910bd9804f32cc31e39d
Alexander F. Rødseth <rodseth@gmail.com>
2022-08-26 17:25:39 +0200

    Add an ulimit check to the welcome script, ref #118

--- welcome.sh
+++ welcome.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
+
+min_ulimit=512
+ulimit=$(ulimit -n 2>/dev/null || echo "not found")
+
+if [ "$ulimit" = "not found" ]; then
+  echo 'NOTE:'
+  echo "  'ulimit -n' could not be run."
+  echo "  'ulimit -n' returns the number of files that can be open at the same time."
+  echo "  The '--autorefresh' flag will not work if the ulimit is too low."
+  exit 1
+fi
+
+if [ "$ulimit" -lt "$min_ulimit" ]; then
+  echo 'NOTE:'
+  echo "  'ulimit -n' on your system returns $ulimit."
+  echo "  'ulimit -n' returns the number of files that can be open at the same time."
+  echo "  $ulimit is too low to be able to use the '--autorefresh' flag."
+  echo "  Increase the ulimit on your system to at least $min_ulimit to be able to use it."
+  exit 1
+fi
+
 echo 'Try editing the markdown file in "samples" directory and see the'
 echo 'results instantly in the browser at http://localhost:3000/'
+
 ./algernon --dev --conf serverconf.lua --dir samples --httponly --debug --autorefresh --bolt --server "$@"
