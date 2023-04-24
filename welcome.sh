2dee29978ef1926892be2466c1407a395792a7f8
Alexander F. Rødseth <rodseth@gmail.com>
2023-04-24 19:53:26 +0200

    Add a check for macOS Ventura

--- welcome.sh
+++ welcome.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
 min_ulimit=512
-ulimit=$(ulimit -n 2>/dev/null || echo "not found")
+ulimit=$(ulimit -n 2> /dev/null || echo "not found")
+kernmax=$(sysctl kern.maxfiles 2> /dev/null || echo "not found")
 
-if [ "$ulimit" = "not found" ]; then
-  echo 'NOTE:'
-  echo "  'ulimit -n' could not be run."
-  echo "  'ulimit -n' returns the number of files that can be open at the same time."
-  echo "  The '--autorefresh' flag will not work if the ulimit is too low."
-  exit 1
-fi
-
-if [ "$ulimit" -lt "$min_ulimit" ]; then
-  echo 'NOTE:'
-  echo "  'ulimit -n' on your system returns $ulimit."
-  echo "  'ulimit -n' returns the number of files that can be open at the same time."
-  echo "  $ulimit is too low to be able to use the '--autorefresh' flag."
-  echo "  Increase the ulimit on your system to at least $min_ulimit to be able to use it."
-  exit 1
+if [ "$kernmax" = "kern.maxfiles: 245760" ]; then
+  true # probably Ventura
+else
+  if [ "$ulimit" = "not found" ]; then
+    echo 'NOTE:'
+    echo "  'ulimit -n' could not be run."
+    echo "  'ulimit -n' returns the number of files that can be open at the same time."
+    echo "  The '--autorefresh' flag will not work if the ulimit is too low."
+    exit 1
+  fi
+  if [ "$ulimit" -lt "$min_ulimit" ]; then
+    echo 'NOTE:'
+    echo "  'ulimit -n' on your system returns $ulimit."
+    echo "  'ulimit -n' returns the number of files that can be open at the same time."
+    echo "  $ulimit is too low to be able to use the '--autorefresh' flag."
+    echo "  Increase the ulimit on your system to at least $min_ulimit to be able to use it."
+    exit 1
+  fi
 fi
 
 echo 'Try editing the markdown file in "samples" directory and see the'
