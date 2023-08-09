484764029200827b8e02940aa0c7898abc9e54c3
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-09 16:40:25 +0200

    Fix one of the benchmarking scripts

--- bench/stress_server.sh
+++ bench/stress_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-go build -mod=vendor && ./algernon -n -t -c --cachesize=10000000 --nolimit --cpuprofile=algernon.prof . :7531
+go build -mod=vendor -tags=trace && ./algernon -n -t -c --cachesize=10000000 --nolimit --cpuprofile=algernon.prof . :7531
 
 # The "-race" flag gets in the way of the CPU profiling, unfortunately
 #go build -mod=vendor -race && ./algernon -n -t -c --cachesize=10000000 --nolimit . :7531
