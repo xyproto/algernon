96fc51014ef07138f06a9ee235ecf274963ed09a
Alexander F. Rødseth <rodseth@gmail.com>
2024-08-07 17:13:57 +0200

    Add a comment to a script

--- patchdep.sh
+++ patchdep.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# To make GOOS=linux GOARCH=arm GOARM=7 also work:
+# To make GOOS=linux GOARCH=arm GOARM=7 also work, see https://github.com/pingcap/tidb/pull/55237
 sed -i 's/length  int$/length  int64/g' vendor/github.com/pingcap/tidb/pkg/parser/mysql/util.go
 sed -i 's/(flen int,/(flen int64,/g' vendor/github.com/pingcap/tidb/pkg/parser/mysql/util.go
 sed -i 's/type SQLMode int/type SQLMode int64/g' vendor/github.com/pingcap/tidb/pkg/parser/mysql/const.go
