f1cf72517e4e08c0b258a4840ff5fa62eaf3675d
Alexander F. Rødseth <rodseth@gmail.com>
2025-01-07 11:24:05 +0100

    Update a comment

--- engine/quic_enabled.go
+++ engine/quic_enabled.go
@@ -16,10 +16,11 @@ const quicEnabled = true
 // ListenAndServeQUIC attempts to serve the given http.Handler over QUIC/HTTP3,
 // then reports back any errors when done serving.
 func (ac *Config) ListenAndServeQUIC(mux http.Handler, justServeRegularHTTP chan bool, servingHTTPS *atomic.Bool) {
-	// TODO: Handle ctrl-c by fetching the quicServer struct and passing it to GenerateShutdownFunction.
-	//       This can be done once CloseGracefully in h2quic has been implemented:
-	//       https://github.com/lucas-clemente/quic-go/blob/master/h2quic/server.go#L257
-	// TODO: As far as I can tell, this was never implemented. Look into implementing this for github.com/xyproto/quic
+	// TODO: * Handle ctrl-c by fetching the quicServer struct and passing it to GenerateShutdownFunction.
+	//         This can be done once CloseGracefully in h2quic has been implemented:
+	//         https://github.com/lucas-clemente/quic-go/blob/master/h2quic/server.go#L257
+	//       * As far as I can tell, this was never implemented. Look into implementing this for github.com/xyproto/quic
+	//       * See also: https://github.com/quic-go/quic-go/blob/3cb5f3e104d269768415cce79ddcc5018c79ea92/integrationtests/self/http_shutdown_test.go#L36
 	//
 	// gracefulServer.ShutdownInitiated = ac.GenerateShutdownFunction(nil, quicServer)
 	if err := http3.ListenAndServeTLS(ac.serverAddr, ac.serverCert, ac.serverKey, mux); err != nil {
