044b9f41cec00c9bd19de9816fe1338e305c62da
Alexander F. Rødseth <rodseth@gmail.com>
2025-02-08 14:50:03 +0100

    Check if the connection has been closed in the typewriter sample

--- samples/typewriter/index.lua
+++ samples/typewriter/index.lua
@@ -40,8 +40,11 @@ print([[
 function typewriter(text, charDelay, sentenceDelay)
   for i = 1, #text do
     local c = text:sub(i, i)
-    print_nonl(c)   -- Print character without a newline.
-    flush()         -- Flush the output so the character appears immediately.
+    print_nonl(c)        -- Print character without a newline.
+    could_flush = flush()         -- Flush the output so the character appears immediately.
+    if not could_flush then
+      return true
+    end
     sleep(charDelay)
     if c == '.' or c == '!' or c == '?' then
       sleep(sentenceDelay)
@@ -50,14 +53,17 @@ function typewriter(text, charDelay, sentenceDelay)
       print("<br>")
     end
   end
+  return false
 end
 
 -- Example usage:
-typewriter("Hello, world.\n\nThis is a server-side typewriter effect test that uses flush() and print_nonl().\n\nMay the 4th be with you!\n", 0.05, 0.5)
+connection_closed = typewriter("Hello, world.\n\nThis is a server-side typewriter effect test that uses flush() and print_nonl().\n\nMay the 4th be with you!\n", 0.05, 0.5)
 
--- Close the HTML container
-print([[
-</div>
-</body>
-</html>
-]])
+if not connection_closed then
+  -- Close the HTML container
+  print([[
+  </div>
+  </body>
+  </html>
+  ]])
+end
