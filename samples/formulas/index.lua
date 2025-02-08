713e3145370dd45e6b114ae10dc5eb91aeade809
Alexander F. Rødseth <rodseth@gmail.com>
2025-02-08 15:10:37 +0100

    Improve the mathematical formula sample

--- samples/formulas/index.lua
+++ samples/formulas/index.lua
@@ -1,16 +1,18 @@
 -- Function to read and render *.md files in the script directory
 local function render_markdown_files()
     local md_files = readglob("*.md")
+    local additionalJS = ""
     for i = 1, #md_files do
         local content = md_files[i]
         if content then
             print('<div class="formula-box">')
-            mprint(content)
+            additionalJS = mprint_ret(content)
             print('</div>')
         else
             print('<div class="formula-box"><p>Error: Unable to read markdown file</p></div>')
         end
     end
+    return additionalJS
 end
 
 -- Main function
@@ -29,11 +31,18 @@ local function main()
     ]]
 
     -- Render markdown files
-    render_markdown_files()
+    additionalScriptTag = render_markdown_files()
 
     print[[
             </div>
         </div>
+    ]]
+
+    if additionalScriptTag ~= "" then
+      print(additionalScriptTag)
+    end
+
+    print[[
     </body>
     </html>
     ]]
