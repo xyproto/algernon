5aae6fc060a000ac02b1f18d80622d2edcedd6d2
Alexander F. Rødseth <rodseth@gmail.com>
2025-11-18 16:29:18 +0100

    Append concurrently to access logs

--- engine/access.go
+++ engine/access.go
@@ -22,9 +22,8 @@ func (ac *Config) CommonLogFormat(req *http.Request, statusCode int, byteSize in
 		username = ac.perm.UserState().Username(req)
 	}
 	host, _, err := net.SplitHostPort(req.RemoteAddr)
-	ip := host
 	if err != nil {
-		ip = req.RemoteAddr
+		host = req.RemoteAddr
 	}
 	statusCodeString := "-"
 	if statusCode > 0 {
@@ -35,7 +34,7 @@ func (ac *Config) CommonLogFormat(req *http.Request, statusCode int, byteSize in
 		byteSizeString = strconv.FormatInt(byteSize, 10)
 	}
 	timestamp := strings.Replace(time.Now().Format("02/Jan/2006 15:04:05 -0700"), " ", ":", 1)
-	return fmt.Sprintf("%s - %s [%s] \"%s %s %s\" %s %s", ip, username, timestamp, req.Method, req.RequestURI, req.Proto, statusCodeString, byteSizeString)
+	return fmt.Sprintf("%s - %s [%s] \"%s %s %s\" %s %s", host, username, timestamp, req.Method, req.RequestURI, req.Proto, statusCodeString, byteSizeString)
 }
 
 // CombinedLogFormat returns a line with the data that is available at the start
@@ -48,9 +47,8 @@ func (ac *Config) CombinedLogFormat(req *http.Request, statusCode int, byteSize 
 		username = ac.perm.UserState().Username(req)
 	}
 	host, _, err := net.SplitHostPort(req.RemoteAddr)
-	ip := host
 	if err != nil {
-		ip = req.RemoteAddr
+		host = req.RemoteAddr
 	}
 	statusCodeString := "-"
 	if statusCode > 0 {
@@ -63,36 +61,38 @@ func (ac *Config) CombinedLogFormat(req *http.Request, statusCode int, byteSize 
 	timestamp := strings.Replace(time.Now().Format("02/Jan/2006 15:04:05 -0700"), " ", ":", 1)
 	referer := req.Header.Get("Referer")
 	userAgent := req.Header.Get("User-Agent")
-	return fmt.Sprintf("%s - %s [%s] \"%s %s %s\" %s %s \"%s\" \"%s\"", ip, username, timestamp, req.Method, req.RequestURI, req.Proto, statusCodeString, byteSizeString, referer, userAgent)
+	return fmt.Sprintf("%s - %s [%s] \"%s %s %s\" %s %s \"%s\" \"%s\"", host, username, timestamp, req.Method, req.RequestURI, req.Proto, statusCodeString, byteSizeString, referer, userAgent)
 }
 
 // LogAccess creates one entry in the access log, given a http.Request,
 // a HTTP status code and the amount of bytes that have been transferred.
 func (ac *Config) LogAccess(req *http.Request, statusCode int, byteSize int64) {
 	if ac.commonAccessLogFilename != "" {
-		f, err := os.OpenFile(ac.commonAccessLogFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
-		if err != nil {
-			logrus.Warnf("Can not open %s: %s", ac.commonAccessLogFilename, err)
-			return
-		}
-		defer f.Close()
-		_, err = f.WriteString(ac.CommonLogFormat(req, statusCode, byteSize) + "\n")
-		if err != nil {
-			logrus.Warnf("Can not write to %s: %s", ac.commonAccessLogFilename, err)
-			return
-		}
+		go func(filename string) {
+			f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+			if err == nil { // success
+				defer f.Close()
+				_, err = f.WriteString(ac.CommonLogFormat(req, statusCode, byteSize) + "\n")
+				if err != nil {
+					logrus.Warnf("Can not write to %s: %s", filename, err)
+				}
+			} else {
+				logrus.Warnf("Can not open %s: %s", filename, err)
+			}
+		}(ac.commonAccessLogFilename)
 	}
 	if ac.combinedAccessLogFilename != "" {
-		f, err := os.OpenFile(ac.combinedAccessLogFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
-		if err != nil {
-			logrus.Warnf("Can not open %s: %s", ac.combinedAccessLogFilename, err)
-			return
-		}
-		defer f.Close()
-		_, err = f.WriteString(ac.CombinedLogFormat(req, statusCode, byteSize) + "\n")
-		if err != nil {
-			logrus.Warnf("Can not write to %s: %s", ac.combinedAccessLogFilename, err)
-			return
-		}
+		go func(filename string) {
+			f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+			if err == nil { // success
+				defer f.Close()
+				_, err = f.WriteString(ac.CombinedLogFormat(req, statusCode, byteSize) + "\n")
+				if err != nil {
+					logrus.Warnf("Can not write to %s: %s", filename, err)
+				}
+			} else {
+				logrus.Warnf("Can not open %s: %s", filename, err)
+			}
+		}(ac.combinedAccessLogFilename)
 	}
 }
