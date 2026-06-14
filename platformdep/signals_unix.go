ebe166dcab7cf45c0c1df420bf3a534c6c02d269
KozakLordOfMatrix <matrixneo2026@tutamail.com>
2026-06-14 22:12:52 +0300

    feat: allow SIGUSR2 to clear the cache

--- platformdep/signals_unix.go
+++ platformdep/signals_unix.go
@@ -8,12 +8,12 @@ import (
 	"syscall"
 )
 
-// SetupSignals installs a SIGUSR1 handler that invokes clearCacheFunction in
+// SetupSignals installs SIGUSR1/SIGUSR2 handlers that invoke clearCacheFunction in
 // a goroutine. printFunction is used to log the received signal.
 func SetupSignals(clearCacheFunction func(), printFunction func(format string, args ...any)) {
-	// Listen for SIGUSR1 to clear the cache
+	// Listen for SIGUSR1 and SIGUSR2 to clear the cache
 	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGUSR1)
+	signal.Notify(signals, syscall.SIGUSR1, syscall.SIGUSR2)
 	go func() {
 		for {
 			// Wait for a signal of the type given to signal.Notify
